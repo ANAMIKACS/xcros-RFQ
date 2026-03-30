807a78d44c817c9674313ac980a181eb24b369a3
Ashitha <ashithaajith04@gmail.com>
2026-03-30 15:39:34 +0530

    updated listing

--- src/app/rfq-listing/rfq-listing.ts
+++ src/app/rfq-listing/rfq-listing.ts
@@ -99,12 +99,22 @@ isPincodeBoxOpen=false;
 togglePincodeBoxOpen(){
   this.isPincodeBoxOpen=!this.isPincodeBoxOpen;
 }
+//Budget Box
+isBudgetBoxOpen=false;
+toggleBudgetBoxOpen(){
+  this.isBudgetBoxOpen=!this.isBudgetBoxOpen;
+}
+//property Status
+isStatusBoxOpen=false;
+toggleStatusBoxOpen(){
+  this.isStatusBoxOpen=!this.isStatusBoxOpen;
+}
  //MEDIA DROPDOWN
  value = '';
  isFeaturedOpen = false;
-  selectedFeatured = 'Featured';
+  selectedFeatured = 'Sort By';
   featuredOptions = [
-    'Rating H>L',
+    'Oldest First',
     'Rating H>L',
     'Rating H>L',
     'Rating H>L',
@@ -116,4 +126,121 @@ togglePincodeBoxOpen(){
     this.selectedFeatured = option;
     this.isFeaturedOpen = false;
   }
+
+  //Features
+  dashboardData = [
+  {
+    icon: '/listingpage/campaign-icon.png',
+    value: '20+',
+    title: 'Campaign'
+  },
+  {
+    icon: '/listingpage/Rfq-icon.png',
+    value: '10+',
+    title: "RFQ's"
+  },
+  {
+    icon: '/listingpage/Replies-icon.png',
+    value: '100+',
+    title: 'Replies'
+  },
+  {
+    icon: '/listingpage/Interest-icon.png',
+    value: '2000+',
+    title: 'Interests'
+  },
+  {
+    icon: '/listingpage/bots-icon.png',
+    value: '2000+',
+    title: 'Bots'
+  }
+];
+//All Listing
+isAllListingOpen= false;
+toggleListing(){
+  this.isAllListingOpen= !this.isAllListingOpen;
+}
+//Media box
+  isMediaOpen = false;
+toggleMedia() {
+  this.isMediaOpen = !this.isMediaOpen;
+}
+//Categories box
+  isCategoriesOpen = false;
+toggleCategories() {
+  this.isCategoriesOpen = !this.isCategoriesOpen;
+}
+//Statistics box
+  isStatisticsOpen = false;
+toggleStatistics() {
+  this.isStatisticsOpen = !this.isStatisticsOpen;
+}
+listings = [
+  {
+    image: '/listingpage/lisiting-img.png',
+    user: 'User Name',
+    date: '20 Jan, 2026',
+    type: 'Apartment',
+    city: 'Chennai',
+    location: 'T.Nagar',
+    loan: 'Yes',
+    loanPercent: '50%'
+  },
+  {
+    image: '/listingpage/lisiting-img.png',
+    user: 'User Name',
+    date: '20 Jan, 2026',
+    type: 'Apartment',
+    city: 'Chennai',
+    location: 'T.Nagar',
+    loan: 'Yes',
+    loanPercent: '50%'
+  },
+   {
+    image: '/listingpage/lisiting-img.png',
+    user: 'User Name',
+    date: '20 Jan, 2026',
+    type: 'Apartment',
+    city: 'Chennai',
+    location: 'T.Nagar',
+    loan: 'Yes',
+    loanPercent: '50%'
+  },
+   {
+    image: '/listingpage/lisiting-img.png',
+    user: 'User Name',
+    date: '20 Jan, 2026',
+    type: 'Apartment',
+    city: 'Chennai',
+    location: 'T.Nagar',
+    loan: 'Yes',
+    loanPercent: '50%'
+  }
+];
+ marketingItems = [
+    'Digital Marketing',
+    'SMS',
+    'Whatsapp',
+    'Email',
+    'Calls',
+    'Print',
+    'Social Media Marketing',
+    'Influencer marketing'
+  ];
+ categories = [
+    { name: 'Plot', value: 0 },
+    { name: 'Apartment', value: 0 },
+    { name: 'Villa', value: 0 },
+    { name: 'Individual', value: 0 },
+    { name: 'Shop', value: 0 }
+  ];
+  statistics = [
+    { title: 'Campaigns', value: 0 },
+    { title: 'Users', value: 0 },
+    { title: 'Posts', value: 0 },
+    { title: 'Seller Replies', value: 0 },
+    { title: 'User Replies', value:0},
+    { title: 'tags', value:0},
+  ];
+description= "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  olore magna aliqua. Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, consectetur adipiscin, Lorem sum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  olore magna aliqua. Lorem ipsum dolor sit amet," 
 }
